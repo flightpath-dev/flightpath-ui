8cdba0cb6d019b6a6c675a2b29f194b1e85a15fd
Naresh Bhatia <me@nareshbhatia.dev>
2026-01-25 12:07:04 -0500

    clean up ESLint configuration (#28)

--- packages/config-eslint/vite.js
+++ packages/config-eslint/vite.js
@@ -1,11 +1,11 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
-import globals from "globals";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import importX from "eslint-plugin-import-x";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import importX from "eslint-plugin-import-x";
-import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
-import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import ts from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -13,7 +13,7 @@ export default defineConfig([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
+      ts.configs.recommended,
       importX.flatConfigs.recommended,
       importX.flatConfigs.typescript,
       reactHooks.configs.flat.recommended,
